f0deac59ae95526dd9ff6cadb9c44c6a42860470
huijie <huijie@hudongpai.com>
2017-08-12 19:06:50 +0800

    遮罩层位置修改

--- src/components/packages/images-select/src/app.vue
+++ src/components/packages/images-select/src/app.vue
@@ -9,13 +9,13 @@
                 <div class="item-setting">
                     <button class="setting-btn">更换</button>
                     <button class="setting-btn">删除</button>
-                    <button class="setting-btn" @click="showDialog = true">裁剪</button>
+                    <button class="setting-btn" @click="fixThumbnailImg">裁剪</button>
                 </div>
             </div>
         </div>
         
         <!-- TODO 此处可抽离做组件 -->
-        <div class="is-dialog" v-if="showDialog">
+        <div class="is-dialog select-dialog" v-if="showDialog">
             <div class="dialog__wrapper" @click="showDialog = false">
             </div>
             <div class="dialog__contain">
@@ -24,10 +24,15 @@
                     <span class="header-close" @click="showDialog = false">X</span>
                 </div>
                 <div class="body">
-                    <canvas class="thumbnail-canvas"></canvas>
+                    <canvas class="thumbnail-canvas" id="fixCanvas"
+                        @mousedown="fixImgStart" 
+                        @mousemove="fixImg"
+                        @mouseup="fixImgEnd"
+                    ></canvas>
+                    <div class="holder" id="holderBox"></div>
                 </div>
                 <div class="footer">
-                    <button class="is-button">取消</button>
+                    <button class="is-button" @click="showDialog = false">取消</button>
                     <button class="is-button">重选</button>
                     <button class="is-button">确认使用</button>
                 </div>
@@ -45,21 +50,70 @@
                     name: "img1.jpg", 
                     url: require("img/upload/img1.jpg")
                 },
-                showDialog: true
+                showDialog: false,
+                // 裁剪图片操作
+                isFixImg: false,
+                fixImgPos: {
+                    start: {
+                        x: 0, y: 0
+                    },
+                    end: {
+                        x: 0, y: 0
+                    }
+                },
             }
         },
         mounted(){
-            this.$nextTick(()=>{
-                this.setImg();
-            })
+            // this.$nextTick(()=>{
+            //     this.setImg();
+            // })
         },
         methods: {
             setImg(){
-                let canvas = $(".thumbnail-canvas")[0], ctx = canvas.getContext("2d");
+                let canvas = $("#fixCanvas")[0], ctx = canvas.getContext("2d");
                 let img = document.getElementById("thumbnailImg");
 
-                ctx.drawImage(img, 0, 0, 349, 600);
-                // ctx.drawImage(img, 33, 71, 104, 124, 21, 20, 87, 104)
+                this.fixImgPos.end = {
+                    x: $(canvas).offset().left + $(canvas).width(),
+                    y: $(canvas).offset().top + $(canvas).height()
+                }
+                ctx.drawImage(img, 0, 0, $(canvas).height(), $(canvas).width());
+            },
+            fixThumbnailImg(){
+                this.showDialog = true;
+                this.$nextTick(()=>{
+                    this.setImg();
+                })                
+            },
+            fixImgStart(evt){
+                this.isFixImg = true;
+
+                this.fixImgPos.start = {
+                    x: evt.pageX,
+                    y: evt.pageY
+                }
+            },
+            fixImg(evt){
+                if( this.isFixImg ){
+                    this.fixImgPos.end = {
+                        x: evt.pageX, y: evt.pageY
+                    }
+                    let canvas = $("#fixCanvas");
+
+                    $("#holderBox").css({
+                        width: this.fixImgPos.end.x - this.fixImgPos.start.x,
+                        height: this.fixImgPos.end.y - this.fixImgPos.start.y,
+                        left: this.fixImgPos.start.x + 20 - canvas.offset().left,
+                        top: this.fixImgPos.start.y + 20 - canvas.offset().top,
+                    })
+                }
+            },
+            fixImgEnd(evt){
+                this.fixImgPos.end = {
+                    x: evt.pageX,
+                    y: evt.pageY
+                }
+                this.isFixImg = false;
             }
         }
     }
